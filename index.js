743028e53f592432565e1760d0cabe52982e688a
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-06-22 22:25:30 +0300

    adds index.js after using find

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+
+function superbowlWin(recordArray) {
+  // Use find to get the first object with result "W"
+  const win = recordArray.find(record => record.result === "W");
+
+  // If a win is found, return the year; otherwise return undefined
+  return win ? win.year : undefined;
+}
